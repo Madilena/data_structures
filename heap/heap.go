380e5fbf18d1a1e1e4b6e087a7ec964a3e25ac1c
Madilena <madilenam@gmail.com>
2022-01-08 18:12:35 -0800

    heapify array

--- heap/heap.go
+++ heap/heap.go
@@ -6,21 +6,84 @@ import (
 )
 
 func main() {
-	array := []int{100, 3, 25, 1}
+	array := []int{100, 3, 25, 29, 52, 1, 8, 9, 11, 18, 19, 69, 75, 92, 22}
 	array_length := len(array)
-	for i := 0; i < array_length; i++ {
+	//startIndex is last non-leaf node
+	startIndex := array_length/2 - 1
+	fmt.Println(array)
+	//sift down from last non-leaf node
+	for i := startIndex; i >= 0; i-- {
 		heapify(&array, i)
 	}
-	fmt.Println(array)
+	fmt.Println("number of tree levels: ", levelOrder(len(array)))
 
+	heapAsTreeBasic(&array)
+	printHeapAsTree(&array)
 }
 
-func powerOfTwo(exponent int) int {
-	return int(math.Pow(2, float64(exponent)))
+func heapify(heap *[]int, i int) {
+	smallest := i
+	lChildIndex := leftChild(i)
+	rChildIndex := rightChild(i)
+
+	if childIsSmaller(lChildIndex, heap, smallest) {
+		smallest = lChildIndex
+	}
+
+	if childIsSmaller(rChildIndex, heap, smallest) {
+		smallest = rChildIndex
+	}
+
+	if smallest != i {
+		swapCurrentSmallestWithChild(heap, i, smallest)
+		heapify(heap, smallest)
+	}
 }
 
-func depthOfTree(exponent int) int {
-	return int(math.Pow(2, float64(exponent)))
+func heapAsTree(heap *[]int) [][]int {
+	order := levelOrder(len(*heap))
+	orderLevelArray := [][]int{}
+	for i := 0; i <= order; i++ {
+		bushArray := []int{}
+		if i == 0 {
+			continue
+		}
+		for j := powerOfTwo(i-1) - 1; j < powerOfTwo(i)-1; j++ {
+			if j < len(*heap) {
+				bushArray = append(bushArray, (*heap)[j])
+			}
+		}
+		orderLevelArray = append(orderLevelArray, bushArray)
+	}
+	//fmt.Println("this is the order array:", orderLevelArray)
+	return orderLevelArray
+}
+
+func printHeapAsTree(heap *[]int) {
+	order := levelOrder(len(*heap))
+	for i := 0; i < order; i++ {
+		fmt.Println(heapAsTree(heap)[i])
+	}
+}
+
+func heapAsTreeBasic(heap *[]int) {
+	order := levelOrder(len(*heap))
+	for i := 0; i <= order; i++ {
+		if i == 0 {
+			continue
+		}
+		for j := powerOfTwo(i-1) - 1; j < powerOfTwo(i)-1; j++ {
+			if j < len(*heap) {
+				fmt.Println("tree level is ", i, "array index is: ", j, "node value is ", (*heap)[j], " ")
+			}
+		}
+	}
+}
+
+func levelOrder(arrayLength int) int {
+	binLog := math.Log2(float64(arrayLength))
+	roundUp := math.Ceil(binLog)
+	return int(roundUp)
 }
 
 func leftChild(i int) int {
@@ -31,19 +94,27 @@ func rightChild(i int) int {
 	return 2*i + 2
 }
 
-func heapify(heap *[]int, i int) {
-	smallest := i
-	lChild := leftChild(i)
-	rChild := rightChild(i)
-	if lChild < len(*heap) && (*heap)[lChild] < (*heap)[smallest] {
-		smallest = lChild
-	}
+func childIsSmaller(child int, heap *[]int, smallest int) bool {
+	return childIsWithinArray(child, heap) && childIsLessThanSmallest(child, heap, smallest)
+}
 
-	if rChild < len(*heap) && (*heap)[rChild] < (*heap)[smallest] {
-		smallest = rChild
-	}
-	if smallest != i {
-		(*heap)[i], (*heap)[smallest] = (*heap)[smallest], (*heap)[i]
-		heapify(heap, smallest)
-	}
+func childIsWithinArray(child int, heap *[]int) bool {
+	return child < len(*heap)
+}
+
+func childIsLessThanSmallest(child int, heap *[]int, smallest int) bool {
+	return (*heap)[child] < (*heap)[smallest]
+}
+
+func swapCurrentSmallestWithChild(heap *[]int, i int, smallest int) {
+	(*heap)[i], (*heap)[smallest] = (*heap)[smallest], (*heap)[i]
+	fmt.Println((*heap))
+}
+
+func powerOfTwo(exponent int) int {
+	return int(math.Pow(2, float64(exponent)))
+}
+
+func depthOfTree(exponent int) int {
+	return int(math.Pow(2, float64(exponent)))
 }
---
+++ heap/heap.md
@@ -0,0 +1,71 @@
+# Heap Notes
+
+These are my misc. notes on the heap data structure
+
+## What is a heap?
+
+- Heap says, 'I got a bunch of values and I just want to keep track of the min or max value'
+- A heap is not a sorted structure. It can be regarded as partially ordered. It's possible to have different heaps for the same set of elements.
+- A heap is a complete binary tree (all levels completely filled except maybe last level)
+- A heap is backed by an array
+- Heap does not allow duplicates
+- Heap order property : For min heap, parent is less than its children. For max heap, parent is greater than its children.
+
+## Big O for heap
+
+This explains [optimizing heap building](https://stackoverflow.com/questions/9755721/how-can-building-a-heap-be-on-time-complexity)
+
+Notice that only one node is at the top whereas half the nodes lie in the bottom layer. So, if we have to apply an operation to every node we would prefer to start at the end of the array and sift down (towards index=0) over siftUp. startIndex is last non-leaf node (array_length/2 - 1)
+
+## Details on running heap.go
+
+```
+example
+
+starting with this array:
+{100, 3, 25, 29, 52, 1, 8, 9, 11, 18, 19, 69, 75, 92, 22}
+
+we create the min heap:
+[1 3 8 9 18 25 22 29 11 52 19 69 75 92 100]
+
+we can display how the sift down finds the min value
+
+[100 3 25 29 52 1 8 9 11 18 19 69 75 92 22]
+[100 3 25 29 18 1 8 9 11 52 19 69 75 92 22]
+[100 3 25 9 18 1 8 29 11 52 19 69 75 92 22]
+[100 3 1 9 18 25 8 29 11 52 19 69 75 92 22]
+[1 3 100 9 18 25 8 29 11 52 19 69 75 92 22]
+[1 3 8 9 18 25 100 29 11 52 19 69 75 92 22]
+[1 3 8 9 18 25 22 29 11 52 19 69 75 92 100]
+
+we can also get additional info about the heap nodes:
+
+number of tree levels:  4
+tree level is  1 array index is:  0 node value is  1
+tree level is  2 array index is:  1 node value is  3
+tree level is  2 array index is:  2 node value is  8
+tree level is  3 array index is:  3 node value is  9
+tree level is  3 array index is:  4 node value is  18
+tree level is  3 array index is:  5 node value is  25
+tree level is  3 array index is:  6 node value is  22
+tree level is  4 array index is:  7 node value is  29
+tree level is  4 array index is:  8 node value is  11
+tree level is  4 array index is:  9 node value is  52
+tree level is  4 array index is:  10 node value is  19
+tree level is  4 array index is:  11 node value is  69
+tree level is  4 array index is:  12 node value is  75
+tree level is  4 array index is:  13 node value is  92
+tree level is  4 array index is:  14 node value is  100
+
+
+we can also display the min heap as a tree
+
+[1]
+[3 8]
+[9 18 25 22]
+[29 11 52 19 69 75 92 100]
+```
+
+## Our example with image
+
+![Alt text](heap_example.png?raw=true"Title")
